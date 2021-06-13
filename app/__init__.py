ef839afba33268f42a6d396228d04360daf8b46c
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-13 12:39:52 +0300

    Navbar done.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,5 +14,8 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Flask extensions
+    bootstrap.init_app(app)
 
     return app
