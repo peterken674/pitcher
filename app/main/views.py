9df51ae8d5c7da5f6a589d49aef2f30c8f8a8ed7
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-15 19:32:53 +0300

    Change import path.

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from app.models import Category, Pitch, Comment
+from ..models import Category, Pitch, Comment
 from flask import render_template, redirect, url_for
 from . import main
 from flask_login import login_required, current_user
